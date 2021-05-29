e61bd7eba4df978b9e9cde38680c9069ead95c68
goodlucky1215 <goodlucky1215@naver.com>
2021-05-30 03:02:28 +0900

    어노테이션으로 IOC,DI 해보기

---
+++ spring4_1_1/src/local/di2/Sonata.java
@@ -0,0 +1,50 @@
+package local.di2;
+
+import org.springframework.stereotype.Component;
+
+//import org.springframework.context.annotation.Scope;
+
+@Component("myCar")
+public class Sonata{
+   private String carColor = null;
+   private int wheelNum = 0;
+   private int speed = 0;
+   public Sonata() {
+	   System.out.println("짜짠~~!");
+   }
+   public Sonata(String carColor, int wheelNum) {
+      this.carColor = carColor;
+      this.wheelNum = wheelNum;
+   }
+   public Sonata(String carColor, int wheelNum, int speed) {
+      this.carColor = carColor;
+      this.wheelNum = wheelNum;
+      this.speed = speed;
+   }
+   @Override
+   public String toString() {
+      return "자동차 정보 출력하기";
+   }
+   public String getCarColor() {
+      return carColor;
+   }
+   public int getWheelNum() {
+      return wheelNum;
+   }
+   public int getSpeed() {
+      return speed;
+   }
+   public void setCarColor(String carColor) {
+	   System.out.println("차 색을 결정");
+      this.carColor = carColor;
+   }
+   public void setWheelNum(int wheelNum) {
+	   System.out.println("차 번호를 결정");
+      this.wheelNum = wheelNum;
+   }
+   public void setSpeed(int speed) {
+	   System.out.println("차 스피드ㄹ를 결정");
+      this.speed = speed;
+   }
+
+}
---
+++ spring4_1_1/src/local/di2/SonataApx.java
@@ -0,0 +1,17 @@
+package local.di2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SonataApx {
+	public static void main(String[] args) {
+		//Sonata herCar = new Sonata(); //라이프 사이클 관리를 직접해야한다. => 인스턴스화 (우리는 이거를 직접 관리 안 시키려고!)
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("local\\di2\\sonataBean.xml");
+		/*
+		 * Sonata myCar = null; //처음에는 null로 관리를 해야 모범 답안 myCar =
+		 * (Sonata)context.getBean("myCar"); System.out.println(myCar.toString());
+		 * System.out.println("my의 현재 속도"+myCar.getWheelNum());
+		 */
+	}
+}
