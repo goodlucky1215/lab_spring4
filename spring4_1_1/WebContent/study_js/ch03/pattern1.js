2eb2ba04ccb48e4701bfeeb937f0050ac88cc4d3
goodlucky1215 <goodlucky1215@naver.com>
2021-06-08 13:18:01 +0900

    선언적 함수, 익명 함수 사용해보기

---
+++ spring4_1_1/WebContent/study_js/ch03/pattern1.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//제공 함수를 통한 패턴 분석1
+{
+  //console.log('test');
+  let addNum = 0;
+  let subNum = 100;
+
+  const auto_1 = setInterval(function(){
+    addNum++;
+    console.log("addNum: "+addNum);
+  },3000);
+
+  const auto_2 = setInterval(function(){
+    subNum--;
+    console.log("subNum: "+subNum);
+  },3000);
+
+}
+
+//node pattern1.js 라고 터미널에 치면 실행이 됨
---
+++ spring4_1_1/WebContent/study_js/ch03/pattern2.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//제공 함수를 통한 패턴 분석1
+{
+  //console.log('test');
+  let addNum = 0;
+
+  const auto_1 = setTimeout(function(){
+    addNum++;
+    console.log("addNum: "+addNum);
+  },3000);
+
+}
